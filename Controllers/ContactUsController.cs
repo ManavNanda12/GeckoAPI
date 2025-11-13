9abb636310c9c1356800e11b623ebecf76ab20fc
DESKTOP-CR240EQ\Evince <manavevince@gmail.com>
2025-11-13 15:14:01 +0530

    feat: Contact us module add on

--- Controllers/ContactUsController.cs
+++ Controllers/ContactUsController.cs
@@ -1,4 +1,5 @@
 ﻿using DemoWebAPI.model.Models;
+using GeckoAPI.Common;
 using GeckoAPI.Model.models;
 using GeckoAPI.Service.contactus;
 using GeckoAPI.Service.dashboard;
@@ -14,12 +15,18 @@ namespace GeckoAPI.Controllers
     {
         #region Fields
         private readonly IContactusService _contactusService;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly EmailService _emailService;
+        private readonly IWebHostEnvironment _env;
         #endregion
 
         #region Constructor
-        public ContactUsController(IContactusService contactusService)
+        public ContactUsController(IContactusService contactusService , IHttpContextAccessor httpContextAccessor, EmailService emailService , IWebHostEnvironment env)
         {
             _contactusService = contactusService;
+            _env = env;
+            _emailService = emailService;
+            _httpContextAccessor = httpContextAccessor;
         }
         #endregion
 
@@ -27,13 +34,13 @@ namespace GeckoAPI.Controllers
         /// <summary>
         /// Get contact us list
         /// </summary>
-        [HttpGet("get-contactus-list")]
-        public async Task<BaseAPIResponse<List<ContactUs>>> GetContactUsList()
+        [HttpPost("get-contactus-list")]
+        public async Task<BaseAPIResponse<List<ContactUs>>> GetContactUsList(CommonListRequestModel model)
         {
             var response = new BaseAPIResponse<List<ContactUs>>();
             try
             {
-                var contactusList = await _contactusService.GetContactUsList();
+                var contactusList = await _contactusService.GetContactUsList(model);
 
                 // Set the response data
                 response.Data = contactusList;
@@ -43,6 +50,58 @@ namespace GeckoAPI.Controllers
             catch (Exception ex)
             {
                 // Handle exceptions and set error response
+                response.Success = false;
+                response.Message = $"An error occurred: {ex.Message}";
+            }
+            return response;
+        }
+
+        /// <summary>
+        /// Save contact us request
+        /// </summary>        
+        [HttpPost("save-contactus-request")]
+        public async Task<BaseAPIResponse<long>> SaveWishlistItem(ContactUs model)
+        {
+            var response = new BaseAPIResponse<long>();
+            try
+            {
+
+                var isAdded = await _contactusService.SaveContactRequest(model);
+                if (isAdded == 1)
+                {
+                    response.Success = true;
+                    response.Message = "Contact us replied successfully.";
+                    string filePath = Path.Combine(_env.WebRootPath, "EmailTemplates", "CustomerReplyTemplate.html");
+                    string htmlTemplate = await System.IO.File.ReadAllTextAsync(filePath);
+
+                    string htmlBody = htmlTemplate
+                        .Replace("{{CustomerName}}", model.CustomerName)
+                        .Replace("{{OriginalSubject}}", model.ContactSubject)
+                        .Replace("{{OriginalDate}}", DateTime.Now.ToString("MMMM dd, yyyy"))
+                        .Replace("{{OriginalMessage}}", model.CustomerMessage)
+                        .Replace("{{ReplyMessage}}", model.AdminMessage)
+                        .Replace("{{AdminName}}", "Manav")
+                        .Replace("{{AdminTitle}}", "CEO")
+                        .Replace("{{AdminEmail}}", "manav@evincedev.com")
+                        .Replace("{{AdminPhone}}", "8780160945")
+                        .Replace("{{Year}}", DateTime.Now.Year.ToString());
+
+                    await _emailService.SendEmailAsync(model.CustomerEmail, model.CustomerName, "Response to Your Inquiry - FitLife Gym", htmlBody);
+                    await _emailService.SendEmailAsync("manav@evincedev.com", "Manav", "Provided response to request", htmlBody);
+                }
+                else if (isAdded == 0)
+                {
+                    response.Success = true;
+                    response.Message = "Thanks for contacting";
+                }
+                else
+                {
+                    response.Success = false;
+                    response.Message = "Failed to submit contact us request.";
+                }
+            }
+            catch (Exception ex)
+            {
                 response.Success = false;
                 response.Message = $"An error occurred: {ex.Message}";
             }
--- CustomerControllers/ContactUsController.cs
+++ CustomerControllers/ContactUsController.cs
@@ -1,4 +1,5 @@
 ﻿using DemoWebAPI.model.Models;
+using GeckoAPI.Common;
 using GeckoAPI.Model.models;
 using GeckoAPI.Service.contactus;
 using GeckoAPI.Service.wishlist;
@@ -13,12 +14,18 @@ namespace GeckoAPI.CustomerControllers
     {
         #region Fields
         private readonly IContactusService _contactusService;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly EmailService _emailService;
+        private readonly IWebHostEnvironment _env;
         #endregion
 
         #region Constructor
-        public ContactUsController(IContactusService contactusService)
+        public ContactUsController(IContactusService contactusService, IWebHostEnvironment env, IHttpContextAccessor httpContextAccessor, EmailService emailService)
         {
             _contactusService = contactusService;
+            _env = env;
+            _emailService = emailService;
+            _httpContextAccessor = httpContextAccessor;
         }
         #endregion
 
@@ -43,6 +50,28 @@ namespace GeckoAPI.CustomerControllers
                 {
                     response.Success = true;
                     response.Message = "Thanks for contacting";
+                    string thanksFilePath = Path.Combine(_env.WebRootPath, "EmailTemplates", "CustomerThankYouTemplate.html");
+                    string thanksHtmlTemplate = await System.IO.File.ReadAllTextAsync(thanksFilePath);
+
+                    string thanksHtmlBody = thanksHtmlTemplate
+                        .Replace("{{CustomerName}}", model.CustomerName)
+                        .Replace("{{Year}}", DateTime.Now.Year.ToString());
+                    await _emailService.SendEmailAsync(model.CustomerEmail, model.CustomerName, "Thank You for Contacting Us", thanksHtmlBody);
+
+
+                    string notifyFilePath = Path.Combine(_env.WebRootPath, "EmailTemplates", "AdminNotificationTemplate.html");
+                    string notifyHtmlTemplate = await System.IO.File.ReadAllTextAsync(notifyFilePath);
+
+                    string notifyHtmlBody = notifyHtmlTemplate
+                        .Replace("{{CustomerName}}", model.CustomerName)
+                        .Replace("{{CustomerEmail}}", model.CustomerEmail)
+                        .Replace("{{Subject}}", model.ContactSubject)
+                        .Replace("{{Message}}", model.CustomerMessage)
+                        .Replace("{{DateTime}}", DateTime.Now.ToString("MMMM dd, yyyy hh:mm tt"))
+                        .Replace("{{Year}}", DateTime.Now.Year.ToString());
+
+                    await _emailService.SendEmailAsync("manav@evincedev.com", "Admin", "New Customer Inquiry", notifyHtmlBody);
+
                 }
                 else
                 {
