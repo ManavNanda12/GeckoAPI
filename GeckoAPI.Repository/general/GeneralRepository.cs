51523b629f5d10bab5009b472bebd206419c7c09
ManavNanda12 <manavnanda2404@gmail.com>
2026-03-24 18:26:19 +0530

    Cleaned repo - removed ignored files

--- GeckoAPI.Repository/general/GeneralRepository.cs
+++ GeckoAPI.Repository/general/GeneralRepository.cs
@@ -2,19 +2,15 @@
 using DemoWebAPI.model.Models;
 using GeckoAPI.Model.models;
 using Microsoft.Extensions.Options;
-using Npgsql;
 using Dapper;
 
 namespace GeckoAPI.Repository.general
 {
     public class GeneralRepository : BaseRepository, IGeneralRepository
     {
-        private readonly string _connectionString;
-
         #region Constructor
         public GeneralRepository(IOptions<DbConfig> config) : base(config)
         {
-            _connectionString = config.Value.DefaultConnection;
         }
         #endregion
 
@@ -22,30 +18,24 @@ namespace GeckoAPI.Repository.general
 
         public Task<List<Country>> GetAllCountries()
         {
-            using var connection = new NpgsqlConnection(_connectionString);
-            string sql = $"SELECT * FROM {StoredProcedures.GetCountryList.ToLower()}()";
-            var data = connection.Query<Country>(sql).ToList();
-            return Task.FromResult(data);
+            var response = Query<Country>(StoredProcedures.GetCountryList);
+            return Task.FromResult(response.Data.ToList());
         }
 
         public Task<List<State>> GetStatesByCountryId(long countryId)
         {
-            using var connection = new NpgsqlConnection(_connectionString);
             var param = new DynamicParameters();
             param.Add("CountryId", countryId);
-            string sql = $"SELECT * FROM {StoredProcedures.GetStateList.ToLower()}(@CountryId)";
-            var data = connection.Query<State>(sql, param).ToList();
-            return Task.FromResult(data);
+            var response = Query<State>(StoredProcedures.GetStateList, param);
+            return Task.FromResult(response.Data.ToList());
         }
 
         public Task<List<City>> GetCitiesByStateId(long stateId)
         {
-            using var connection = new NpgsqlConnection(_connectionString);
             var param = new DynamicParameters();
             param.Add("StateId", stateId);
-            string sql = $"SELECT * FROM {StoredProcedures.GetCityList.ToLower()}(@StateId)";
-            var data = connection.Query<City>(sql, param).ToList();
-            return Task.FromResult(data);
+            var response = Query<City>(StoredProcedures.GetCityList, param);
+            return Task.FromResult(response.Data.ToList());
         }
 
         #endregion
